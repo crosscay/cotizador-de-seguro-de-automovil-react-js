449d8545a27107968ddb4d7dffaf4286ee3cc2c1
crosscay <caicedocarlos0306@gmail.com>
2020-08-10 21:34:53 -0500

    Updates

---
+++ precache-manifest.72ec12ebd2fcbe42b422c2c0419c0d9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7773d586416d847ca6b977e55b569ea",
+    "url": "/cotizador_de_seguro_de_automovil_react/index.html"
+  },
+  {
+    "revision": "e36a89002c3bfc845619",
+    "url": "/cotizador_de_seguro_de_automovil_react/static/css/main.4cc8a829.chunk.css"
+  },
+  {
+    "revision": "1f2a16e349a5745c259b",
+    "url": "/cotizador_de_seguro_de_automovil_react/static/js/2.0fd8c2dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotizador_de_seguro_de_automovil_react/static/js/2.0fd8c2dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36a89002c3bfc845619",
+    "url": "/cotizador_de_seguro_de_automovil_react/static/js/main.3a6d5d58.chunk.js"
+  },
+  {
+    "revision": "cc23c98a435dc7861cc6",
+    "url": "/cotizador_de_seguro_de_automovil_react/static/js/runtime-main.a8260b77.js"
+  }
+]);
